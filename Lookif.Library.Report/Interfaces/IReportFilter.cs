5349d1dabebd813a9450d00666fc348515491d2f
sfarshadhn <sfarshadhn@outlook.com>
2022-06-25 11:09:52 +0430

    Support null value

--- Lookif.Library.Report/Interfaces/IReportFilter.cs
+++ Lookif.Library.Report/Interfaces/IReportFilter.cs
@@ -11,37 +11,47 @@ public interface IReportFilter
 {
     public List<ReportFilter> GetReportFilter()
     {
-        var listOfFilter = new List<ReportFilter>();
-        var lst = this.GetProperties();
-        foreach (var item in lst)
+        try
         {
-            var type = item.PropertyType;
-            var data = item.GetValue(this, null)!;
+            var listOfFilter = new List<ReportFilter>();
+            var lst = this.GetProperties();
+            foreach (var item in lst)
+            {
+                var type = item.PropertyType;
+                var data = item.GetValue(this, null)!;
+                if (data is null)
+                    continue;
+                var value = TypeDescriptor.GetConverter(type).ConvertFromInvariantString(data?.ToString());
+                var defaultValue = (type == typeof(string)) ? "" : Activator.CreateInstance(type);
+                if (value.Equals(defaultValue))
+                    continue;
+                if (type == typeof(DateTime))
+                    listOfFilter.Add(new ReportFilter()
+                    {
+                        Property = item.Name.ToString(),
+                        Value = value.ToString(),
+                        FilterFunction = ExpressionFunction.EqualDateTime
+                    });
 
-            var value = TypeDescriptor.GetConverter(type).ConvertFromInvariantString(data?.ToString());
-            var defaultValue = (type == typeof(string)) ? "" : Activator.CreateInstance(type);
-            if (value.Equals(defaultValue))
-                continue;
-            if (type == typeof(DateTime))
-                listOfFilter.Add(new ReportFilter()
-                {
-                    Property = item.Name.ToString(),
-                    Value = value.ToString(),
-                    FilterFunction = ExpressionFunction.EqualDateTime
-                });
-
-            else if (value.ToString() != defaultValue.ToString())
-                listOfFilter.Add(new ReportFilter()
-                {
-                    Property = item.Name.ToString(),
-                    Value = value.ToString(),
-                    FilterFunction = (type == typeof(string)) ? ExpressionFunction.Contains : ExpressionFunction.Equal,
-                    ExpressionRelation = ExpressionFunction.And
-                });
+                else if (value.ToString() != defaultValue.ToString())
+                    listOfFilter.Add(new ReportFilter()
+                    {
+                        Property = item.Name.ToString(),
+                        Value = value.ToString(),
+                        FilterFunction = (type == typeof(string)) ? ExpressionFunction.Contains : ExpressionFunction.Equal,
+                        ExpressionRelation = ExpressionFunction.And
+                    });
 
 
+            }
+            return listOfFilter;
         }
-        return listOfFilter;
+        catch (Exception e)
+        {
+
+            throw;
+        }
+      
 
     }
 }
